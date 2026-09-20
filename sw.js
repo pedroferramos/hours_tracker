80a20410b9567d1d2b471e2fea7bee0d5cc040f6
Pedro Ramos <162768375+pedroferramos@users.noreply.github.com>
2026-09-20 08:27:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE when you change files so phones pick up the update.
-const CACHE = 'weekly-hours-v5';
+const CACHE = 'weekly-hours-v6';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
